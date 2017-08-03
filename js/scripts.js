3e34f6e28af662dd5ec16ba4ff87ce1e81ee3c87
Melissa Pabst <melissapabst@gmail.com>
2017-08-02 20:57:01 -0500

    modified variable attribute names

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@ var book1 = {
     "id": 1,
     "name": "Adorable Puppies",
     "author": "Binkey McGillocuddy"
-    "picture_url": "http://wallpapercave.com/wp/N2lOzO4.jpg",
+    "picture": "http://wallpapercave.com/wp/N2lOzO4.jpg",
     "price": 9.95,
-    "selling_points": [
+    "selling-points": [
         "Pugs Rule",
         "Pugs are Sweet",
         "Everybody Loves Pugs"
@@ -15,9 +15,9 @@ var book2 = {
     "id": 2,
     "name": "Granite City Soul Suckers",
     "author": "Bella Lugosi"
-    "picture_url": "http://medias.photodeck.com/abc3ec2e-e46d-11e0-8813-0175b4d176d6/100514_IND_003_xgaplus.jpg",
+    "picture": "http://medias.photodeck.com/abc3ec2e-e46d-11e0-8813-0175b4d176d6/100514_IND_003_xgaplus.jpg",
     "price": 2.00,
-    "selling_points": [
+    "selling-points": [
         "If you Haven't Been, Don't Go",
         "Get Mugged Fast",
         "Steal Your Car"
@@ -27,9 +27,9 @@ var book3 = {
     "id": 3,
     "name": "JavaScript is SAVVY",
     "author": "Benevolent Dictator"
-    "picture_url": "https://www.codementor.io/assets/page_img/learn-javascript.png",
+    "picture": "https://www.codementor.io/assets/page_img/learn-javascript.png",
     "price": 45.99,
-    "selling_points": [
+    "selling-points": [
         "Learn JavaScript",
         "Survive Your First ReFactor",
         "Free Coffee"
@@ -39,9 +39,9 @@ var book4 = {
     "id": 4,
     "name": " Monty Python (the Expurgated Version)",
     "author": "Noel HamWrapper"
-    "picture_url": "http://media.topito.com/wp-content/uploads/2014/10/python1.jpg",
+    "picture": "http://media.topito.com/wp-content/uploads/2014/10/python1.jpg",
     "price": 30.00,
-    "selling_points": [
+    "selling-points": [
         "Toupee Spam Muppet",
         "Decal Slide Off My Car",
         "Whatever You Can Steal"
