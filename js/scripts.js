4478b0a3ca7ae0539c30d89a5cf5e8eb2cad3524
Melissa Pabst <melissapabst@gmail.com>
2017-08-02 21:00:12 -0500

    fixed some syntax

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 var book1 = {
     "id": 1,
     "name": "Adorable Puppies",
-    "author": "Binkey McGillocuddy"
+    "author": "Binkey McGillocuddy",
     "picture": "http://wallpapercave.com/wp/N2lOzO4.jpg",
     "price": 9.95,
     "selling-points": [
@@ -10,11 +10,11 @@ var book1 = {
         "Pugs are Sweet",
         "Everybody Loves Pugs"
     ]
-}
+};
 var book2 = {
     "id": 2,
     "name": "Granite City Soul Suckers",
-    "author": "Bella Lugosi"
+    "author": "Bella Lugosi",
     "picture": "http://medias.photodeck.com/abc3ec2e-e46d-11e0-8813-0175b4d176d6/100514_IND_003_xgaplus.jpg",
     "price": 2.00,
     "selling-points": [
@@ -22,11 +22,11 @@ var book2 = {
         "Get Mugged Fast",
         "Steal Your Car"
     ]
-}
+};
 var book3 = {
     "id": 3,
     "name": "JavaScript is SAVVY",
-    "author": "Benevolent Dictator"
+    "author": "Benevolent Dictator",
     "picture": "https://www.codementor.io/assets/page_img/learn-javascript.png",
     "price": 45.99,
     "selling-points": [
@@ -34,11 +34,11 @@ var book3 = {
         "Survive Your First ReFactor",
         "Free Coffee"
     ]
-}
+};
 var book4 = {
     "id": 4,
     "name": " Monty Python (the Expurgated Version)",
-    "author": "Noel HamWrapper"
+    "author": "Noel HamWrapper",
     "picture": "http://media.topito.com/wp-content/uploads/2014/10/python1.jpg",
     "price": 30.00,
     "selling-points": [
@@ -46,4 +46,4 @@ var book4 = {
         "Decal Slide Off My Car",
         "Whatever You Can Steal"
     ]
-}
+};
