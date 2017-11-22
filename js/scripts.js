5411a1301ab65086177dbb24c161ce868cf8d67c
Alex Pearson <alex@alexpear.com>
2017-11-21 21:12:35 -0600

    Populate first book's HTML through jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ var book1 = {
   "name": "The Desire Map",
   "author": "Danielle LaPorte",
   "pictureUrl":"https://images-na.ssl-images-amazon.com/images/I/41BhS87QXrL._SX258_BO1,204,203,200_.jpg",
-  "price": "$6.32",
+  "price": 6.32,
   "sellingPoints": [
     "Self Help",
     "Inspiration",
@@ -18,7 +18,7 @@ var book2 = {
   "name": "Savvy Coding For Dummies",
   "author": "Alex Pearson",
   "pictureUrl":"https://images-na.ssl-images-amazon.com/images/I/51q-YHB62GL._SX258_BO1,204,203,200_.jpg",
-  "price": "$1799.00",
+  "price": 1799.00,
   "sellingPoints": [
     "Tech Help",
     "Web Design",
@@ -31,7 +31,7 @@ var book3 = {
   "name": "The Monster at the End of This Book",
   "author": "Jon Stone",
   "pictureUrl":"http://smollin.com/michael/tmonstr/mon001.jpg",
-  "price": "$6.32",
+  "price": 6.32,
   "sellingPoints": [
     "Timeless",
     "Interactive",
@@ -41,11 +41,22 @@ var book3 = {
 
 
 $('#book1 h1').text(book1.name)
-$('#book1 h2').text(book1.author)
-$('#book1 img').attr({
-  src: book1.pictureURL
-})
-$('#book1 h2:last-child').text(book1.price)
-$('#book1 p:first-child').text(book1.sellingPoints[0])
-$('#book1 p:nth-child(2)').text(book1.sellingPoints[1])
-$('#book1 p:last-child').text(book1.sellingPoints[2])
+$('#book1 h2')
+  .first()
+  .text(book1.author)
+
+$('#book1 img').attr({ src: book1.pictureUrl })
+$('#book1 h2 > span.price').text(book1.price)
+
+$('#book1 p')
+  .first()
+  .text(book1.sellingPoints[0])
+
+$('#book1 p')
+  .first()
+  .next()
+  .text(book1.sellingPoints[1])
+
+$('#book1 p')
+  .last()
+  .text(book1.sellingPoints[2])
