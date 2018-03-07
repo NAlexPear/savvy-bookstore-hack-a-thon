780f8721afafa2c98d2431df22045992725781df
Tony Bush <tbush01@gmail.com>
2018-03-06 20:39:22 -0600

    swap content over from index.html to script.js and inject into html with jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 // Put all of your jQuery and JavaScript in this document.
 var book1 = {
     "id": 1,
-    "title": "50 Shade of Gray",
-    "author": "Garfield"
-    "book-image": "http://graphics8.nytimes.com/images/2015/10/15/dining/15RECIPE20DIN/15RECIPE20DIN-articleLarge.jpg",
+    "title": "butt cakes",
+    "author": "Garfield",
+    "bookImage": "https://d188rgcu4zozwl.cloudfront.net/content/B007J4T2G8/resources/469708731",
     "price": 17.69,
     "sellingPoints": [
         "Lasagna is delicious.",
@@ -15,8 +15,8 @@ var book1 = {
 var book2 = {
     "id": 2,
     "title": "50 Shade of Gray",
-    "author": "Garfield"
-    "book-image": "http://graphics8.nytimes.com/images/2015/10/15/dining/15RECIPE20DIN/15RECIPE20DIN-articleLarge.jpg",
+    "author": "Garfield",
+    "bookImage": "https://images-na.ssl-images-amazon.com/images/I/51YUYsBp2CL._SX322_BO1,204,203,200_.jpg",
     "price": 17.69,
     "sellingPoints": [
         "Lasagna is delicious.",
@@ -27,8 +27,8 @@ var book2 = {
 var book3 = {
     "id": 3,
     "title": "50 Shade of Gray",
-    "author": "Garfield"
-    "book-image": "http://graphics8.nytimes.com/images/2015/10/15/dining/15RECIPE20DIN/15RECIPE20DIN-articleLarge.jpg",
+    "author": "Garfield",
+    "bookImage": "https://images-na.ssl-images-amazon.com/images/I/51HhuPD7WhL._SX322_BO1,204,203,200_.jpg",
     "price": 17.69,
     "sellingPoints": [
         "Lasagna is delicious.",
@@ -36,3 +36,29 @@ var book3 = {
         "Real G's move silent, like Lasagna. -Lil Wayne"
     ]
 }
+
+$("#book1 > .book-info > .title", ).text(book1.title);
+$("#book2 > .book-info > .title", ).text(book2.title);
+$("#book3 > .book-info > .title", ).text(book3.title);
+
+$("#book1 > .book-info > .author", ).text(book1.author);
+$("#book2 > .book-info > .author", ).text(book2.author);
+$("#book3 > .book-info > .author", ).text(book3.author);
+
+$("#book1 > .book-info > .price", ).text(book3.price);
+$("#book2 > .book-info > .price", ).text(book3.price);
+$("#book3 > .book-info > .price", ).text(book3.price);
+
+$("#book1 > .book-discription > .sellingPoints", ).text(book1.sellingPoints);
+$("#book2 > .book-discription > .sellingPoints", ).text(book2.sellingPoints);
+$("#book3 > .book-discription > .sellingPoints", ).text(book3.sellingPoints);
+
+$("#book1 > .book-img > img", ).attr({
+  "src": book1.bookImage
+});
+$("#book2 > .book-img > img", ).attr({
+  "src": book2.bookImage
+});
+$("#book3 > .book-img > img", ).attr({
+  "src": book3.bookImage
+});
