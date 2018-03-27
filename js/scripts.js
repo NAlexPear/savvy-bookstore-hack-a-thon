730e7ad92670dbd0cbf93e89ba4adb0099a4cdb2
Malcolm <malcolmdjohnson@gmail.com>
2018-03-26 20:44:57 -0500

    add content to books page

--- js/scripts.js
+++ js/scripts.js
@@ -2,81 +2,79 @@
 
 // Put all of your jQuery and JavaScript in this document.
 var products = {
-    "books": [
-        {
-            "creator": "E. L. James",
-            "image": "https://d188rgcu4zozwl.cloudfront.net/content/B007J4T2G8/resources/469708731",
-            "price": 17.69,
-            "sellingPoints": [
-                "Lasagna is delicious.",
-                "The essential guide to Italian casseroles of all types.",
-                "Real G's move silent, like Lasagna. -Lil Wayne"
-            ],
-            "title": "50 Shades of Grey",
-            "type": "book"
-        },
-        {
-            "creator": "Garfield",
-            "image": "https://images-na.ssl-images-amazon.com/images/I/51YUYsBp2CL._SX322_BO1,204,203,200_.jpg",
-            "price": 17.69,
-            "sellingPoints": [
-                "Lasagna is delicious.",
-                "The essential guide to Italian casseroles of all types.",
-                "Real G's move silent, like Lasagna. -Lil Wayne"
-            ],
-            "title": "50 Shade of Gray",
-            "type": "book"
-        },
-        {
-            "creator": "Garfield",
-            "image": "https://images-na.ssl-images-amazon.com/images/I/51HhuPD7WhL._SX322_BO1,204,203,200_.jpg",
-            "price": 17.69,
-            "sellingPoints": [
-                "Lasagna is delicious.",
-                "The essential guide to Italian casseroles of all types.",
-                "Real G's move silent, like Lasagna. -Lil Wayne"
-            ],
-            "title": "50 Shade of Gray",
-            "type": "book"
-        }
+    "books": [ {
+        "creator": "E. L. James",
+        "image": "https://d188rgcu4zozwl.cloudfront.net/content/B007J4T2G8/resources/469708731",
+        "price": 17.69,
+        "sellingPoints": [
+            "Lasagna is delicious.",
+            "The essential guide to Italian casseroles of all types.",
+            "Real G's move silent, like Lasagna. -Lil Wayne"
+        ],
+        "title": "50 Shades of Grey",
+        "type": "book"
+    },
+    {
+        "creator": "Garfield",
+        "image": "https://images-na.ssl-images-amazon.com/images/I/51YUYsBp2CL._SX322_BO1,204,203,200_.jpg",
+        "price": 17.69,
+        "sellingPoints": [
+            "Lasagna is delicious.",
+            "The essential guide to Italian casseroles of all types.",
+            "Real G's move silent, like Lasagna. -Lil Wayne"
+        ],
+        "title": "50 Shade of Gray",
+        "type": "book"
+    },
+    {
+        "creator": "Garfield",
+        "image": "https://images-na.ssl-images-amazon.com/images/I/51HhuPD7WhL._SX322_BO1,204,203,200_.jpg",
+        "price": 17.69,
+        "sellingPoints": [
+            "Lasagna is delicious.",
+            "The essential guide to Italian casseroles of all types.",
+            "Real G's move silent, like Lasagna. -Lil Wayne"
+        ],
+        "title": "50 Shade of Gray",
+        "type": "book"
+    }
     ],
-    "albums": [
-        {
-            "creator": "Nas",
-            "image": "https://audioxide.com/wp-content/uploads/2016/06/Illmatic-500x500.jpg",
-            "price": 9.99,
-            "sellingPoints": [
-                "Lasagna is delicious.",
-                "The essential guide to Italian casseroles of all types.",
-                "Real G's move silent, like Lasagna. -Lil Wayne"
-            ],
-            "title": "Illmatic",
-            "type": "album"
-        },
-        {
-            "creator": "2Pac",
-            "image": "http://3.bp.blogspot.com/_5A6fSMDiXaI/TRGA65YolHI/AAAAAAAAAmo/NknnnYHa0II/s1600/2Pac_-_All_Eyez_on_Me_single_version.jpg",
-            "price": 9.99,
-            "sellingPoints": [
-                "Lasagna is delicious.",
-                "The essential guide to Italian casseroles of all types.",
-                "Real G's move silent, like Lasagna. -Lil Wayne"
-            ],
-            "title": "All Eyez On Me",
-            "type": "album"
-        },
-        {
-            "creator": "Milli Vanilli",
-            "image": "https://direct.rhapsody.com/imageserver/images/Alb.117181854/500x500.jpg",
-            "price": 9.99,
-            "sellingPoints": [
-                "Lasagna is delicious.",
-                "The essential guide to Italian casseroles of all types.",
-                "Real G's move silent, like Lasagna. -Lil Wayne"
-            ],
-            "title": "Girl You Know It's True",
-            "type": "album"
-        }
+    "albums": [ {
+        "creator": "Nas",
+        "image": "https://audioxide.com/wp-content/uploads/2016/06/Illmatic-500x500.jpg",
+        "price": 9.99,
+        "sellingPoints": [
+            "Lasagna is delicious.",
+            "The essential guide to Italian casseroles of all types.",
+            "Real G's move silent, like Lasagna. -Lil Wayne"
+        ],
+        "title": "Illmatic",
+        "type": "album"
+    },
+    {
+        "creator": "2Pac",
+        "image": "http://3.bp.blogspot.com/_5A6fSMDiXaI/TRGA65YolHI/AAAAAAAAAmo/NknnnYHa0II/s1600/2Pac_-_All_Eyez_on_Me_single_version.jpg",
+        "price": 9.99,
+        "sellingPoints": [
+            "Lasagna is delicious.",
+            "The essential guide to Italian casseroles of all types.",
+            "Real G's move silent, like Lasagna. -Lil Wayne"
+        ],
+        "title": "All Eyez On Me",
+        "type": "album"
+    },
+    {
+        "creator": "Milli Vanilli",
+        "image": "https://direct.rhapsody.com/imageserver/images/Alb.117181854/500x500.jpg",
+        "price": 9.99,
+        "sellingPoints": [
+            "Lasagna is delicious.",
+            "The essential guide to Italian casseroles of all types.",
+            "Real G's move silent, like Lasagna. -Lil Wayne"
+        ],
+        "title": "Girl You Know It's True",
+        "type": "album"
+    }
     ]
 };
 
@@ -96,7 +94,12 @@ function appendToPage( item ){
 products.books.forEach( appendToPage );
 products.albums.forEach( appendToPage );
 
-$( "#books" ).click( () => console.log( products.books ) );
+$( "#books" ).click( ( event ) => {
+    event.preventDefault();
+    $( "#content" ).html( "" );
+
+    products.books.forEach( appendToPage );
+} );
 $( "#music" ).click( () => console.log( products.albums ) );
 
 $( "form" ).on( "submit", ( event ) => {
@@ -112,7 +115,7 @@ $( "form" ).on( "submit", ( event ) => {
             formObject.sellingPoints.push( field.value );
         }
         else{
-            formObject[ field.name ] = field.value;
+            formObject[field.name] = field.value;
         }
     } );
 
