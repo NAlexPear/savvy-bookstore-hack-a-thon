6b4e85d936207f6c1ee6c99c11fbe579de4a89bf
Zoey <socksmeanie@gmail.com>
2018-05-14 20:11:05 -0500

    Add arrays

--- js/scripts.js
+++ js/scripts.js
@@ -74,8 +74,67 @@ var album3 = {
         "Pass it, my friend"
     ]
 };
-var books = [ "book1", "book2", "book3"]
-var albums = [ "album1", "album2", "album3"]
+var books = [{  "id": 1,
+  "name": "How Not to Scare Kids",
+  "author": "Iceberg Slim",
+  "pictureUrl": "http://cdn3.momsxyz.com/2015/04/image001.jpg",
+  "price": 10,
+  "sellingPoints": [
+      "Hiding In The closet.",
+      "Never Ask a 2yr old for tissue.",
+      "Be A Friend Before A Parent."]},
+      {"id": 2,
+      "name": "Apples to Oranges",
+      "author": "Farmer Fred",
+      "pictureUrl": "http://strongautomotive.com/wp-content/uploads/2014/11/Apple-Orange-2.jpg",
+      "price": 8,
+      "sellingPoints": [
+          "Home Grown Food.",
+          "Processed Food.",
+          "Chitterlings vs. Veggies."]},
+          {"id": 3,
+          "name": "Queens and Kings",
+          "author": "Shaka Zulu",
+          "pictureUrl": "https://i.pinimg.com/originals/a6/f6/f8/a6f6f872fc9ba5cd80d37971b15e7a1c.jpg",
+          "price": 115,
+          "sellingPoints": [
+              "Killing a Tiger With a Twig.",
+              "How to Rule a Nation.",
+              "Wearing the Crown."]}]
+
+var albums = [{
+    "id": 1,
+    "name": "Houses of the Holy",
+    "author": "Led Zeppelin",
+    "pictureUrl": "https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Led_Zeppelin_-_Houses_of_the_Holy.jpg/220px-Led_Zeppelin_-_Houses_of_the_Holy.jpg",
+    "price": 20,
+    "sellingPoints": [
+        "Over the Hills and Far Away/Dancing Days",
+        "D'yer Mak'er/The Crunge",
+        "Released	in 28 March 1973"
+    ]
+},
+{"id": 2,
+"name": "Broke and Famous",
+"author": "Dormtainment",
+"pictureUrl": "https://direct.rhapsody.com/imageserver/images/Alb.69185622/500x500.jpg",
+"price": 3.99,
+"sellingPoints": [
+    "@$$ On the Internet [Explicit]",
+    "Ballin Ona Budget [Explicit]",
+    "Elephant D-Ck (Bonus Track) [Explicit]"
+]},
+{  "id": 3,
+  "name": "Hippie High",
+  "author": "Josie Hill",
+  "pictureUrl": "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/field-of-flowers-jessica-t-hamilton.jpg",
+  "price": 11.99,
+  "sellingPoints": [
+      "Rolling in the daisies",
+      "High on life",
+      "Pass it, my friend"
+  ]}]
+
 
 
 var addToList = function addToList( bookObj ){
