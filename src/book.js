68a6299e1dc0d4c305eefc02fe329fe46d95a611
Kai Idris <kayodeidrisjr@gmail.com>
2018-07-12 21:14:10 -0500

    sellingpoints are javascript

--- src/book.js
+++ src/book.js
@@ -4,11 +4,11 @@ export default function Book(book){
             <h1>${book.name}</h1>
             <h2>${book.author}</h2>
             <h3>${book.price}</h3>
-            <h3>${book.sellingPoints}</h3>
+            <h3>Selling Points</h3>
             <ol>
-                <li>First Point</li>
-                <li>Second Point</li>
-                <li>Third Point</li>
+                <li>${book.sellingPoint[0]}</li>
+                <li>${book.sellingPoint[1]}</li>
+                <li>${book.sellingPoint[2]}</li>
             </ol>
             <img src = "" alt = "">
         </div>
