e4c7fef30c202bef7117082598c9c31b814a9dc5
Kai Idris <kayodeidrisjr@gmail.com>
2018-07-12 20:11:59 -0500

    add links

--- src/footer.js
+++ src/footer.js
@@ -1,6 +1,9 @@
 export default function footer(state){
     return `
     <div id="footer">
+        <ul>
+            <li>Alexbooks@coders.com</li>
+        </ul>
     </div>
 `;
 }
--- src/header.js
+++ src/header.js
@@ -1,6 +1,7 @@
 export default function header(state){
     return `
     <div id="header">
+        <img src="https://images.pexels.com/photos/33283/stack-of-books-vintage-books-book-books.jpg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="">
     </div>
 `;
 }   
