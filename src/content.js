e45a08524cf4fdf3e4a75c88868b74b4a5d7f34f
Siddhi Goswami <gosiddhi03@gmail.com>
2018-07-12 20:26:12 -0500

    Added form

--- src/content.js
+++ src/content.js
@@ -1,6 +1,13 @@
 export default function Content(){
     return `
         <div id="content">
+            <form>
+                <input type="text" name="title" placeholder="Title">
+                <input type="text" name="author" placeholder="Author">
+                <input type="text" name="pictureUrl" placeholder="Cover Picture">
+                <input type="text" name="price" placeholder="Price">
+                <input type="text" name="sellingPoints" placeholder="Selling Points">
+            </form>
         </div>
     `;
 }
