7777789a8a8d2da6072b7ef610f105026013d098
Alex Pearson <alex@alexpear.com>
2018-07-12 20:36:30 -0500

    Fix Book capitalization

--- src/book.js
+++ src/book.js
@@ -1,17 +1,17 @@
-export default function book(){
+export default function Book(){
     return `
-    <div>
-        <h1>Book</h1>
-        <h2>Author</h2>
-        <h3>Price</h3>   
-        <h3>Selling Points</h3>
-        <ol>
-            <li>First Point</li>
-            <li>Second Point</li>
-            <li>Third Point</li>
-        </ol>    
-        <img src = "" alt = "">
-    </div>            
-`;
-}           
+        <div>
+            <h1>Book</h1>
+            <h2>Author</h2>
+            <h3>Price</h3>
+            <h3>Selling Points</h3>
+            <ol>
+                <li>First Point</li>
+                <li>Second Point</li>
+                <li>Third Point</li>
+            </ol>
+            <img src = "" alt = "">
+        </div>
+    `;
+}
 
