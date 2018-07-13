c7406f7b216556927519b6a9b2c43def290d0779
Kai Idris <kayodeidrisjr@gmail.com>
2018-07-12 21:20:24 -0500

    image is js now

--- src/book.js
+++ src/book.js
@@ -6,11 +6,11 @@ export default function Book(book){
             <h3>${book.price}</h3>
             <h3>Selling Points</h3>
             <ol>
-                <li>${book.sellingPoint[0]}</li>
-                <li>${book.sellingPoint[1]}</li>
-                <li>${book.sellingPoint[2]}</li>
+                <li>${book.sellingPoints[0]}</li>
+                <li>${book.sellingPoints[1]}</li>
+                <li>${book.sellingPoints[2]}</li>
             </ol>
-            <img src = "" alt = "">
+            <img src = "${book.pictureURL}" alt = "">
         </div>
     `;
 }
