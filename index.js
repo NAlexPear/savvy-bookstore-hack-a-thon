e87e47858711e3ae8a2fc7ed818a4a981c3f926c
Alex Pearson <alex@alexpear.com>
2018-07-12 21:25:31 -0500

    Re-organize into books array

--- index.js
+++ index.js
@@ -4,40 +4,41 @@ import Content from './src/Content';
 import Footer from './src/Footer';
 import Form from './src/Form';
 
-var book2 = {
-    "id": 2,
-    "name": "Little Monsters",
-    "author": "Kara Thomas",
-    "pictureUrl": "https://images-na.ssl-images-amazon.com/images/I/517wZWFjCFL._SX331_BO1,204,203,200_.jpg",
-    "price": 30,
-    "sellingPoints": [
-        "Unexpected Plot twists",
-    ]
- }
+var books = [
+    {
+        "id": 1,
+        "name": "Little Monsters",
+        "author": "Kara Thomas",
+        "pictureUrl": "https://images-na.ssl-images-amazon.com/images/I/517wZWFjCFL._SX331_BO1,204,203,200_.jpg",
+        "price": 30,
+        "sellingPoints": [
+            "Unexpected Plot twists",
+        ]
+    },
+    {
+        "id": 2,
+        "name": "The Go-Giver",
+        "author": "Bob Burg and John David Mann",
+        "pictureUrl": "https://m.media-amazon.com/images/I/51V8p5d7rVL._SL500_.jpg",
+        "price": 13.29,
+        "sellingPoints": [
+            "A Little Story About a Powerful Business Idea"
+        ]
+    },
+    {
+        "id": 3,
+        "name": "Hitchhikers Guide to the Galaxy",
+        "author": "Douglas Adams",
+        "pictureUrl": "https://images.penguinrandomhouse.com/cover/9781400052929",
+        "price": 42,
+        "sellingPoints": [
+            "DON'T PANIC",
+            "You can wave your towel in emergencies as a distress signal",
+            "Sadly, there are no Lil Wayne quotes about this book"
+        ]
+    }
+];
 
- var book3 = {
-    "id": 3,
-    "name": "The Go-Giver",
-    "author": "Bob Burg and John David Mann",
-    "pictureUrl": "https://m.media-amazon.com/images/I/51V8p5d7rVL._SL500_.jpg",
-    "price": 13.29,
-    "sellingPoints": [
-        "A Little Story About a Powerful Business Idea"
-    ]
- }
-
- var book4 = {
-    "id": 4,
-    "name": "Hitchhikers Guide to the Galaxy",
-    "author": "Douglas Adams",
-    "pictureUrl": "https://images.penguinrandomhouse.com/cover/9781400052929",
-    "price": 42,
-    "sellingPoints": [
-        "DON'T PANIC",
-        "You can wave your towel in emergencies as a distress signal",
-        "Sadly, there are no Lil Wayne quotes about this book"
-    ]
-}
 
 var root = document.querySelector('#root');
 
@@ -45,7 +46,7 @@ function render(){
     root.innerHTML = `
         ${Navigation()}
         ${Header()}
-        ${Content()}
+        ${Content(books)}
         ${Form()}
         ${Footer()}
     `;
--- src/content.js
+++ src/content.js
@@ -3,9 +3,9 @@ import Book from './Book'
 export default function Content(books){
     return `
         <div id="content">
-            ${Book()}
-            ${Book()}
-            ${Book()}
+            ${Book(books[0])}
+            ${Book(books[1])}
+            ${Book(books[2])}
         </div>
     `;
 }
