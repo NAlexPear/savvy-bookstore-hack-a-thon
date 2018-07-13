c849a40a371292976e45425eb51866d64e73aa1b
Omar Helal <omsy09@hotmail.com>
2018-07-12 20:28:56 -0500

    Fixes per Alex

--- src/book.js
+++ src/book.js
@@ -1,12 +1,17 @@
 export default function book(){
     return `
+    <div>
         <h1>Book</h1>
+        <h2>Author</h2>
+        <h3>Price</h3>   
+        <h3>Selling Points</h3>
             <ol>
-                <li><img src = "" alt = ""></li>
-                <li>Author</li>
-                <li>Price</li>
-                <li>Selling Points</li>
-            </ol>
+                <li>First Point</li>
+                <li>Second Point</li>
+                <li>Third Point</li>
+            </ol>    
+    <img src = "" alt = "">
+    </div>            
 `;
 }           
 
