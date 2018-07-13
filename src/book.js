4b5e82d4b0a760ef9fbfabc3395e913b387ce13a
Omar Helal <omsy09@hotmail.com>
2018-07-12 20:17:54 -0500

    Html for book infor

---
+++ src/book.js
@@ -0,0 +1,12 @@
+export default function book(){
+    return `
+        <h1>Book</h1>
+            <ol>
+                <li><img src = "" alt = ""></li>
+                <li>Author</li>
+                <li>Price</li>
+                <li>Selling Points</li>
+            </ol>
+`;
+}           
+
