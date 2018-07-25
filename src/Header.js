9c3d9c24315bcc130ef851717dfc38011023b632
Marcus Boyd <mboyd3813@gmail.com>
2018-07-24 21:02:22 -0500

    Title inside of Image CSS

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,8 @@
 export default function Header(){
     return `
-    <div id="header">
-        <img src="https://images.pexels.com/photos/33283/stack-of-books-vintage-books-book-books.jpg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="">
+    <div class="container">
+        <img src="https://images.pexels.com/photos/33283/stack-of-books-vintage-books-book-books.jpg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="" style="width:100%;">
+        <div class="centered">Book A Thon</div>
     </div>
 `;
 }
