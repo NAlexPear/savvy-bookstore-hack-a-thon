b8812f9a13685f1e362e4fe982cde212cea7b81d
Alex Pearson <alex@alexpear.com>
2018-07-24 20:19:40 -0500

    Fix missing comma and event listener location

--- index.js
+++ index.js
@@ -39,26 +39,6 @@ var books = [
     }
 ];
 
-document
-    .querySelector('form')
-    .addEventListener(
-        'submit',
-        (event) => {
-            var data = event.target.elements;
-            var newProduct = {
-                'name': data[0].value,
-                'author': data[1].value,
-                'pictureURL': data[2].value,
-                'price': data[3].value
-                'sellingPoints': []
-            };
-
-            books[books.length] = newProduct;
-
-            render(books);
-        }
-    );
-
 
 var root = document.querySelector('#root');
 
@@ -70,6 +50,26 @@ function render(){
         ${Form()}
         ${Footer()}
     `;
+
+    document
+        .querySelector('form')
+        .addEventListener(
+            'submit',
+            (event) => {
+                var data = event.target.elements;
+                var newProduct = {
+                    'name': data[0].value,
+                    'author': data[1].value,
+                    'pictureURL': data[2].value,
+                    'price': data[3].value,
+                    'sellingPoints': []
+                };
+
+                books[books.length] = newProduct;
+
+                render(books);
+            }
+        );
 }
 
 render();
