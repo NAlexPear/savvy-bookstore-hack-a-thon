cabf6911c6611ef947e7a791f3712472efe08038
LapperMedic <alexrlapp@gmail.com>
2018-09-06 20:12:39 -0500

    Export default Book function

--- components/Book.js
+++ components/Book.js
@@ -1,4 +1,4 @@
-function Book(book) {
+export default function Book(book) {
     return `<div class="book">
                 <img class="cover" src="${book.pictureUrl}">
                 <span class="title">${book.name}</span>
