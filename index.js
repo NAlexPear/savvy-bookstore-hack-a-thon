095743965cfcb0a723731f15b61efa876aceb2df
Nick McVickar <NMcVickar@aol.com>
2018-09-06 19:59:08 -0500

    fixed render function

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ import Content from './components/Content';
 import Footer from './components/Footer';
 
 var book1 = {
-	id: 1,
-	name: 'Fried Chicken - A Guide',
-	author: 'Col Sanders',
-	pictureUrl:
+	"id": 1,
+	"name": 'Fried Chicken - A Guide',
+	"author": 'Col Sanders',
+	"pictureUrl":
 		'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2009/5/27/0/0125629_03_chicken-in-skillet_s4x3.jpg.rend.hgtvcom.616.462.suffix/1371589386937.jpeg',
-	price: 4.99,
-	sellingPoints: [
+	"price": 4.99,
+	"sellingPoints": [
 		"Finger Lickin' Good - Some Guy",
 		"#1 on KFC's Best Seller List",
 		"Don't Buy this Book - Jillian Michaels",
@@ -18,13 +18,13 @@ var book1 = {
 };
 
 var novel = {
-	id: 2,
-	name: 'THe Savvy Life',
-	author: 'Elaine Alex',
-	pictureUrl:
+	"id": 2,
+	"name": 'THe Savvy Life',
+	"author": 'Elaine Alex',
+	"pictureUrl":
 		'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Typing_computer_screen_reflection.jpg/800px-Typing_computer_screen_reflection.jpg',
-	price: 33,
-	sellingPoints: [
+	"price": 33,
+	"sellingPoints": [
 		'Enlightenment into the world of Front-End Web Development',
 		'New York Times Best Seller',
 		'This book is totally awsome - Dr. Suess',
@@ -32,12 +32,12 @@ var novel = {
 };
 
 var book2 = {
-	id: 1,
-	name: 'Becoming Snoop Lion',
-	author: 'Snoop Dizzle',
-	pictureUrl: 'https://i.kym-cdn.com/photos/images/facebook/000/367/035/e34.jpg',
-	price: 42.0,
-	sellingPoints: [
+	"id": 1,
+	"name": 'Becoming Snoop Lion',
+	"author": 'Snoop Dizzle',
+	"pictureUrl": 'https://i.kym-cdn.com/photos/images/facebook/000/367/035/e34.jpg',
+	"price": 42.0,
+	"sellingPoints": [
 		"You'll fall deep into the weeds throught this highly addictive story - Willie Nelson",
 		'High Times Best Seller',
 		'Each Chapter Contains a Different Strain Thoughts - Seth Rogen',
