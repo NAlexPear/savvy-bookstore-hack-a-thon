29b5968f26159578ca7a9b0a22806c010e93e30f
Tori Stake <v.m.stake@gmail.com>
2018-09-06 19:46:57 -0500

    fixed what dear leader requested

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,8 @@
 export default function Footer(){
     return `
     <div id="footer">
-
         <h4>4220 Duncan Ave, St. Louis, MO 63114</h4>
         <h4>(314) 867 - 5309 | customerservice@savvybooks.com</h4>
-        
     </div>
     `;
 }
--- components/Navigation.js
+++ components/Navigation.js
@@ -1,7 +1,6 @@
 export default function Navigation(){
     return `
     <div id="navigation">
-
     <ul>
         <li>Login</li>
         <li>Search</li>
@@ -10,7 +9,6 @@ export default function Navigation(){
         <li>Music</li>
         <li>Toys & Games</li>
     </ul>
-
     </div>
     `;
 }
