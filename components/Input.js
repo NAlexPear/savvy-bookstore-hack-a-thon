68ed41de2fd7e9585763ef311fd31e7f9a7ca63d
Tori Stake <v.m.stake@gmail.com>
2018-09-17 19:54:48 -0500

    adjusted selling points

--- components/Input.js
+++ components/Input.js
@@ -1,11 +1,12 @@
 export default function Input() {
     return `
             <form action="" method="POST" id="bookInput">
+                
                 <input type="text" name="title" placeholder="Title" required>
                 <input type="text" name="creator" placeholder="Author" required>
                 <input type="text" name="price" placeholder="Price" required>
                 <input type="text" name="image" placeholder="Cover image URL" required>
-                <textarea name="sellingPoints" rows="8" cols="40" placeholder="Blurbs and selling points" reqiured></textarea>
+                <textarea name="selling_points" rows="8" cols="40" placeholder="Blurbs and selling points" reqiured></textarea>
                 <div id="radio-buttons">
                     <input type="radio" name="type" value="book">
                     Book
