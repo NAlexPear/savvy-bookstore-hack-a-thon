8bd5c33ab834ee69a4ea42de0b076466e43000e4
Tori Stake <v.m.stake@gmail.com>
2018-09-17 19:45:16 -0500

    adjusted input values

--- components/Input.js
+++ components/Input.js
@@ -1,11 +1,19 @@
 export default function Input() {
     return `
             <form action="" method="POST" id="bookInput">
-                <input type="text" name="name" placeholder="Title" required>
-                <input type="text" name="author" placeholder="Author" required>
+                <input type="text" name="title" placeholder="Title" required>
+                <input type="text" name="creator" placeholder="Author" required>
                 <input type="text" name="price" placeholder="Price" required>
-                <input type="text" name="pictureUrl" placeholder="Cover image URL" required>
-                <textarea name="title" rows="8" cols="40" name="sellingPoints" placeholder="Blurbs and selling points" reqiured></textarea>
+                <input type="text" name="image" placeholder="Cover image URL" required>
+                <textarea name="sellingPoints" rows="8" cols="40" placeholder="Blurbs and selling points" reqiured></textarea>
+                <div id="radio-buttons">
+                    <input type="radio" name="type" value="book">
+                    Book
+                    <input type="radio" name="type" value="movie">
+                    Movie 
+                    <input type="radio" name="type" value="music">
+                    Music
+                </div>
                 <input type="submit">
             </form>
             `;
