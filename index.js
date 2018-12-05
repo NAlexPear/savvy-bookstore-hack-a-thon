03883638b9cb9be92f4c13d22efaf6d01e63ad69
Summer <Summerrichards01@gmail.com>
2018-12-04 20:03:49 -0600

    adding import at the top

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import Navigation from './src/Navigation';
+import Header from './src/Header';
+import Content from './src/Content';
+import Footer from './src/Footer';
+
+
+
+
 var root = document.querySelector("#root")
 
 function render(state) {
@@ -8,3 +16,5 @@ function render(state) {
     ${Footer(state)}
     `;
 }
+
+render(state);
