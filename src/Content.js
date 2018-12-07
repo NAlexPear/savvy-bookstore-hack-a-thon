35ba9ea1d72d16752bcd5851a92929a3ea93aa33
Derek Dunwiddie <dunwiddie.derek@gmail.com>
2018-12-06 19:22:37 -0600

    Add book cards to content

--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,16 @@
-export default function Content() {
+import Book from "./Book"
+
+export default function Content(state) {
+    // from state, get array of books
+    // for book in books, send book object to Book.js
+    // add Book.js to Content
+    // send template string HTML to index.js
+    for (let i = 0; i < state.books.length; i++) {
+        var list += Book(state.books[i]);
+    };
     return `
         <div id="content">
-        <p> stuff stuff stuff </p>
+            ${list}
         </div>
     `;
 }
