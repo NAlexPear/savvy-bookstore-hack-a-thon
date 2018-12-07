153561d66298d791538b83df58607b88afbf194d
Summer <Summerrichards01@gmail.com>
2018-12-06 19:57:42 -0600

    idk

--- src/Book.js
+++ src/Book.js
@@ -1,11 +1,26 @@
+function buildBook(book) {
+     var list = '';
+     for (let i = 0; i < book.sellingPoints.length; i++) {
+          list += `
+          <li> 
+          ${book.sellingPoints[i]}
+          </li>
+          `;
+     }
+     return list;
+}
+
 export default function Book(book) {
      return `
 <div class= "book" id = ${book.id}>
-     <h3>${book.name} </h3>
-     <h4> ${book.author} </h4>
-     <img src = "${book.pictureUrl}" alt= "">
-     <h4>${book[book.sellingPoints]}</h4>
-     <h5>${book.price}</h5>
+
+<ul>
+${buildBook(book[book.sellingPoints].book)}
+</ul>
+<h3>${book.name} </h3>
+<h4> ${book.author} </h4>
+<h5>${book.price}</h5>
+<img src = "${book.pictureUrl}" alt= "">
 </div>
 `;
 }
