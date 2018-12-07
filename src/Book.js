5b8d162bd648fc0c9cfb4b1eb4d3751462043296
Summer <Summerrichards01@gmail.com>
2018-12-06 19:13:08 -0600

    making templates

--- src/Book.js
+++ src/Book.js
@@ -1,7 +1,15 @@
 export default function Book(){
      return `
-<div class= "book">
+<div class= "book" id = ${book.id}>
+
+
+<h3>${book.name} </h3>
+<h4> ${book.author} </h4>
+<img src = "${book.pictureURL}" alt= "">
+<h5>${book.price}</h5>
+<h4>${book.sellingPoints} </h4>
 </div>
 `;
 }
+
 
