dc1f64c6b16ffd633c0126bf6f5ce308a52fb0c3
Summer <Summerrichards01@gmail.com>
2018-12-06 19:28:16 -0600

    books

--- src/Book.js
+++ src/Book.js
@@ -1,13 +1,11 @@
-export default function Book(){
+export default function Book(book) {
      return `
 <div class= "book" id = ${book.id}>
-
-
-<h3>${book.name} </h3>
-<h4> ${book.author} </h4>
-<img src = "${book.pictureURL}" alt= "">
-<h5>${book.price}</h5>
-<h4>${book.sellingPoints} </h4>
+     <h3>${book.name} </h3>
+     <h4> ${book.author} </h4>
+     <img src = "${book.pictureUrl}" alt= "">
+     <h4>${book[book.sellingPoints]}</h4>
+     <h5>${book.price}</h5>
 </div>
 `;
 }
