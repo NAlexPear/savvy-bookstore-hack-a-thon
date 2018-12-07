2b91dacf9ce9f2d0dba696adc8fa736594e7fddf
Derek Dunwiddie <dunwiddie.derek@gmail.com>
2018-12-06 19:27:53 -0600

    Remove comments and declare list at beginning of function :bomb:

--- src/Content.js
+++ src/Content.js
@@ -1,12 +1,9 @@
 import Book from "./Book"
 
 export default function Content(state) {
-    // from state, get array of books
-    // for book in books, send book object to Book.js
-    // add Book.js to Content
-    // send template string HTML to index.js
+    var list = ''
     for (let i = 0; i < state.books.length; i++) {
-        var list += Book(state.books[i]);
+        list += Book(state.books[i]);
     };
     return `
         <div id="content">
