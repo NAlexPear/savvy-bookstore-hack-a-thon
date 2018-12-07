afa04ba869485e455dada9148f1084870f0e2d21
Alex Pearson <alex@joinvolley.com>
2018-12-06 20:48:46 -0600

    :pencil: Lint more whitespace

--- src/Book.js
+++ src/Book.js
@@ -14,14 +14,14 @@ function buildBook(sellingPoints){
 
 export default function Book(book){
     return `
-      <div class= "book" id = ${book.id}>
+      <div class="book">
          <ul>
              ${buildBook(book.sellingPoints)}
          </ul>
-         <h3>${book.name} </h3>
-         <h4> ${book.author} </h4>
-         <h5>${book.price}</h5>
-         <img src = "${book.pictureUrl}" alt= "">
+         <h3>${book.name}</h3>
+         <h4>${book.author}</h4>
+         <h5>$${book.price}</h5>
+         <img src="${book.pictureUrl}" alt="${book.name}">
       </div>
     `;
 }
