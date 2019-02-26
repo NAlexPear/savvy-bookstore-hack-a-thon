3212be27b4d6f243765314666e323332df7962fc
Evan Watkins <evanfwatkins@gmail.com>
2019-02-25 19:34:43 -0600

    added a render function, overwriting the html

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 var root = document.querySelector('#root');
+// image we have four compnets that we have from source directory 
+// use these components to overright inner html of root element 
+function render(state){
+    root.innerHTML = `
+    ${Navigation(state)}
+    ${Header(state)}
+    ${Content(state)}
+    ${Footer(state)}
+    `;
+};
