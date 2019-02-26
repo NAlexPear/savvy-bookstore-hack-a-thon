46a1aa91c1c3ca51a198ac01745613a1f5ca17b2
Justin Lautz <lautzjd@gmail.com>
2019-02-25 19:59:33 -0600

    Created SubmitJS

---
+++ submit.js
@@ -0,0 +1,8 @@
+var newProduct = {
+    'name': data[0].value,
+    'author': data[1].value,
+    'pictureURL': data[2].value,
+    'price': data[3].value,
+    'sellingPoints': data[4].value,
+};
+
