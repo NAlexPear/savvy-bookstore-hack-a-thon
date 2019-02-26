f842f585d039280c726ee2ef8f8929953c45fd3a
Amanda Engelman <engelman12990@gmail.com>
2019-02-25 19:48:14 -0600

    Pleeeease add this

--- src/footer.js
+++ src/footer.js
@@ -1,4 +1,6 @@
 export default function Footer(){
     return `
-    <div id ="footer"></div>`;
+    <div id ="footer">
+    <a href="https://twitter.com/ichcheezburger?lang=en">Twitter</a>
+    </div>`;
 }
