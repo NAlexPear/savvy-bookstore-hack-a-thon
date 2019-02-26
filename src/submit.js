b29e302229d73d94c9e34d1d89644890c152aedc
Justin Lautz <lautzjd@gmail.com>
2019-02-25 20:26:12 -0600

    ReCreatedJSFileAddedFormCreatedExportFunction

---
+++ src/submit.js
@@ -0,0 +1,26 @@
+export default function formSubmit() {
+    return `
+    
+<form>
+    
+    <label for="title">Book Title</label>
+    <input type="text" id="title" name="title" required>
+
+    <label for="author">Author</label>
+    <input type="text" id="author" name="author" required>
+
+    <label for="picture url">Book Cover</label>
+    <input type="image" id="pic" name="pic" required>
+
+    <label for="price">Book Price</label>
+    <input type="number" id="price" name="price" required>
+
+    <label for="sellingPoints">Selling Points</label>
+    <input type="text" id="sellingPoint" name="sellingPoint" required>
+
+    <input type="submit" value="Submit">
+    <input type="reset" value="clear">
+    
+</form>
+
+    `}
