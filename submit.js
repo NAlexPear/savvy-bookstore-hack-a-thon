aeb4c43c938aef6df351f420c1c22fe69b5aa0e0
Justin Lautz <lautzjd@gmail.com>
2019-02-25 20:26:58 -0600

    ReCreatedJSFileAddedFormCreatedExportFunction

--- submit.js
+++
@@ -1,8 +0,0 @@
-var newProduct = {
-    'name': data[0].value,
-    'author': data[1].value,
-    'pictureURL': data[2].value,
-    'price': data[3].value,
-    'sellingPoints': data[4].value,
-};
-
