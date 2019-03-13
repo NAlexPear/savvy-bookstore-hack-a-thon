5500d0f9ed25c3e9da97b447ffb4816407cce5ec
Alex Pearson <alex@alexpear.com>
2019-03-12 21:10:00 -0500

    :pencil: Fix Array formatting

--- index.js
+++ index.js
@@ -7,27 +7,30 @@ import axios from 'axios';
 
 var State = {
     'books': [],
-    'albums': [ {
-        'title': 'SLABASF',
-        'creator': 'Kevin Townson feat Justin Lautz',
-        'image': 'http://images6.fanpop.com/image/photos/34400000/Stairway-To-Heaven-classic-r-and-b-music-34425776-400-344.jpg',
-        'price': 99.99,
-        'selling_points': [
-            'Always Strive Forever',
-            'Fire straight up fire',
-            'Global Domination'
-        ] },
-
-    {
-        'title': 'Code of conduct',
-        'creator': 'cody and the cat',
-        'image': 'https://cdn.makeawebsitehub.com/wp-content/uploads/2016/02/learn-code-e1455713167295.jpg',
-        'price': 69.99,
-        'selling_points': [
-            'tough fellas always smell like cheese',
-            'super secure minecraft passwords',
-            'the snack locusts have been poisoned by the deadly figs'
-        ] } ]
+    'albums': [
+        {
+            'title': 'SLABASF',
+            'creator': 'Kevin Townson feat Justin Lautz',
+            'image': 'http://images6.fanpop.com/image/photos/34400000/Stairway-To-Heaven-classic-r-and-b-music-34425776-400-344.jpg',
+            'price': 99.99,
+            'selling_points': [
+                'Always Strive Forever',
+                'Fire straight up fire',
+                'Global Domination'
+            ]
+        },
+        {
+            'title': 'Code of conduct',
+            'creator': 'cody and the cat',
+            'image': 'https://cdn.makeawebsitehub.com/wp-content/uploads/2016/02/learn-code-e1455713167295.jpg',
+            'price': 69.99,
+            'selling_points': [
+                'tough fellas always smell like cheese',
+                'super secure minecraft passwords',
+                'the snack locusts have been poisoned by the deadly figs'
+            ]
+        }
+    ]
 };
 
 
