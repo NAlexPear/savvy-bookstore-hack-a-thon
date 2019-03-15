b51c11b8684a5a7ac44811fc92e603b6d7f14831
Raye Mayberry <rayemayberry@yahoo.com>
2019-03-14 19:12:38 -0500

    get albums from api

--- index.js
+++ index.js
@@ -7,30 +7,7 @@ import axios from 'axios';
 
 var State = {
     'books': [],
-    'albums': [
-        {
-            'title': 'SLABASF',
-            'creator': 'Kevin Townson feat Justin Lautz',
-            'image': 'http://images6.fanpop.com/image/photos/34400000/Stairway-To-Heaven-classic-r-and-b-music-34425776-400-344.jpg',
-            'price': 99.99,
-            'selling_points': [
-                'Always Strive Forever',
-                'Fire straight up fire',
-                'Global Domination'
-            ]
-        },
-        {
-            'title': 'Code of conduct',
-            'creator': 'cody and the cat',
-            'image': 'https://cdn.makeawebsitehub.com/wp-content/uploads/2016/02/learn-code-e1455713167295.jpg',
-            'price': 69.99,
-            'selling_points': [
-                'tough fellas always smell like cheese',
-                'super secure minecraft passwords',
-                'the snack locusts have been poisoned by the deadly figs'
-            ]
-        }
-    ]
+    'albums': []
 };
 
 
@@ -61,5 +38,12 @@ axios
     .get('https://api.savvycoders.com/books')
     .then((response) => {
         State.books = response.data;
+        render(State);
+    });
+
+axios
+    .get('https://api.savvycoders.com/albums')
+    .then((response) => {
+        State.albums = response.data;
         render(State);
     });
