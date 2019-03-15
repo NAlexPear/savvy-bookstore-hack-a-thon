03a5535411612503937b72e59c5c9a2e663b46ef
Raye Mayberry <rayemayberry@yahoo.com>
2019-03-14 20:03:05 -0500

    allow content to return different HTML strings based on the value of filter variable

--- src/Content.js
+++ src/Content.js
@@ -7,9 +7,33 @@ function productsBuilder(productList){
 }
 
 export default function Content(state){
+    var products = state.filter;
+
+    if(products === 'books'){
+        return `
+        <div id ="content">
+          <div>
+            ${productsBuilder(state.books)}
+          </div>
+          ${Form()}
+        </div>
+    `;
+    }
+    else if(products === 'albums'){
+        return `
+        <div id ="content">
+          <div>
+            ${productsBuilder(state.albums)}
+          </div>
+          ${Form()}
+        </div>
+    `;
+    }
+    
     return `
         <div id ="content">
           <div>
+            ${productsBuilder(state.albums)}
             ${productsBuilder(state.books)}
           </div>
           ${Form()}
