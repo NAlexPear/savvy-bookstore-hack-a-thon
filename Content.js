4ba14882072865b38fb7baa847dbb0b45882ba2d
dozenji <douglasg.moore@gmail.com>
2019-05-08 20:39:43 -0500

    add content form

--- Content.js
+++ Content.js
@@ -1,1 +1,10 @@
+<form action="" method="post">
+    <label for=""><input type="text" name="name" id=""></label>
+    <label for=""><input type="text" name="autor" id=""></label>
+    <label for=""><input type="image" src="" alt="" name='pictureUrl'></label>
+    <label for=""><input type="number" name="price" id=""></label>
+    <label for=""><input type="text" id="" name="sellingPoints"></label>
+    
+</form>
+
 
