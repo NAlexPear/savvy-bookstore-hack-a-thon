c54d611ab27ab461ca46bcd6d8eca986cb962d4e
dozenji <douglasg.moore@gmail.com>
2019-05-08 20:44:12 -0500

    redo content into form

--- form.js
+++ form.js
@@ -1,10 +1,11 @@
+export default(state) => `
 <form action="" method="post">
     <label for=""><input type="text" name="name" id=""></label>
     <label for=""><input type="text" name="autor" id=""></label>
     <label for=""><input type="image" src="" alt="" name='pictureUrl'></label>
     <label for=""><input type="number" name="price" id=""></label>
     <label for=""><input type="text" id="" name="sellingPoints"></label>
-    
+    <input type="submit" value="whatever">
 </form>
-
+`
 
