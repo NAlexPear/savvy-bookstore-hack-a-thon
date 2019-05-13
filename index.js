1b38ba5b10b8d3c7ccf78bff0719a03bd2f7a818
Manav Misra <manavm@visionwebsoft.com>
2019-05-13 18:58:23 -0500

    Update index.js
    I closed out the xtra space in 'books' import.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import Header from './components/Header';
 import Navigation from './components/Navigation';
 import Content from './components/Content';
 import Form from '/components/Form';
-import Book from '/components/ Books';
+import Book from '/components/Books';
 import Footer from './components/Footer';
 const root = document.querySelector('#root');
 function render(state){
