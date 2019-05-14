516f6c1b6405cba9d24cdc461bd94f3a52b01f73
dozenji <douglasg.moore@gmail.com>
2019-05-13 20:08:22 -0500

    redo return for the forEach

--- components/Content.js
+++ components/Content.js
@@ -2,5 +2,6 @@ import Book from "Book";
 
 export default (states) => {
 let booksHTML = '';
-return states.books.forEach(book => bookHTML += `<figure>${Book(book)}</figure>`);
+states.books.forEach(book => bookHTML += `<figure>${Book(book)}</figure>`);
+return booksHTML;
 };
