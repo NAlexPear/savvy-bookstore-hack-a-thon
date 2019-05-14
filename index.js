560013604649009d7fbd6b3f998b4fb15955bd0a
dozenji <douglasg.moore@gmail.com>
2019-05-13 19:35:48 -0500

    fix state object

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ import Footer from './components/Footer';
 const root = document.querySelector('#root');
 
 const states = {
-    header:{
+    header: {
         title:""
-},
-    navigation:{
+    },
+    navigation: {
         links=[]
     },
-    books:[{
-    "id": 1,
-    "name": "Lasagna: A Retrospective",
-    "author": "Garfield",
-    "pictureUrl": "http://graphics8.nytimes.com/images/2015/10/15/dining/15RECIPE20DIN/15RECIPE20DIN-articleLarge.jpg",
-    "price": 24,
-    "sellingPoints": [
-    "Lasagna is delicious.",
-    "The essential guide to Italian casseroles of all types.",
-    "Real G's move silent, like Lasagna. -Lil Wayne"
-     ] }]
+    books: [{
+        "id": 1,
+        "name": "Lasagna: A Retrospective",
+        "author": "Garfield",
+        "pictureUrl": "http://graphics8.nytimes.com/images/2015/10/15/dining/15RECIPE20DIN/15RECIPE20DIN-articleLarge.jpg",
+        "price": 24,
+        "sellingPoints": [
+        "Lasagna is delicious.",
+        "The essential guide to Italian casseroles of all types.",
+        "Real G's move silent, like Lasagna. -Lil Wayne"
+     ]}]
 };
     
 
