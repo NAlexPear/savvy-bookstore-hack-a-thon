7432846fa28e60d761c51d5db084686991f1e44f
dozenji <douglasg.moore@gmail.com>
2019-05-13 20:18:41 -0500

    refactor buildBooksHTML function

--- components/Content.js
+++ components/Content.js
@@ -1,7 +1,9 @@
 import Book from "Book";
 
-export default (states) => {
-let booksHTML = '';
-states.books.forEach(book => bookHTML += `<figure>${Book(book)}</figure>`);
-return booksHTML;
-};
+const buildBooksHTML = (states) => {
+    let booksHTML = '';
+    states.books.forEach(book => bookHTML += `<figure>${Book(book)}</figure>`);
+    return booksHTML;
+    };
+
+export default buildBooksHTML;
