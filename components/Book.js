9b888ee4ee5f49596c98676aff2b8e894f53f37b
Your Name <mackenziewbryan@gmail.com>
2019-05-13 20:08:55 -0500

    Update Book.js to include author and price

--- components/Book.js
+++ components/Book.js
@@ -1,6 +1,6 @@
 export default (state) => `
     <img src=${state.pictureUrl} alt="Photo wuz here">
-    <figcaption>${state.name}</figcaption>
+    <figcaption>${state.name}-${state.author}-${state.price}</figcaption>
     <ul>
         <li>${state.sellingPoints[0]}</li>
         <li>${state.sellingPoints[1]}</li>
