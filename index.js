21f3061cf5d33c8aa01b2da560fa3d94abe99c93
Alex Pearson <alex@alexpear.com>
2019-06-03 18:56:54 -0500

    :wrench: Set up external API format

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const books = [
         'author': 'Garfield',
         'pictureUrl': 'http://graphics8.nytimes.com/images/2015/10/15/dining/15RECIPE20DIN/15RECIPE20DIN-articleLarge.jpg',
         'price': 24,
-        'sellingPoints': [
+        'selling_points': [
             'Lasagna is delicious.',
             'The essential guide to Italian casseroles of all types.',
             "Real G's move silent, like Lasagna. -Lil Wayne"
@@ -17,7 +17,7 @@ const books = [
         'author': 'Neal Stephenson',
         'pictureUrl': 'https://images-na.ssl-images-amazon.com/images/I/41K%2Bt2TSopL._SX304_BO1,204,203,200_.jpg',
         'price': 200,
-        'sellingPoints': [
+        'selling_points': [
             'Something something bitcoin',
             'I like this -Elon Musk' ,
         ]
@@ -32,14 +32,20 @@ function Book(book){
             <p> Price:$${book.price}</p>
             <img src="${book.pictureUrl}">
             <ul>
-              ${book.sellingPoints.map((sellingPoint) => `<li>${sellingPoint}</li>`).join('')}
+              ${book.selling_points.map((sellingPoint) => `<li>${sellingPoint}</li>`).join('')}
             </ul>
         </div>
     `;
 }
 
-document.querySelector('#content').innerHTML = books.map(Book).join('');
+const content = document.querySelector('#content');
+
+content.innerHTML = books.map(Book).join('');
 
 document
     .getElementById('navigation')
     .addEventListener('click',(event) => event.preventDefault());
+
+fetch('https://api.savvycoders.com/books')
+    .then((response) => response.json())
+    .then((data) => content.innerHTML += data.map(Book).join(''));
