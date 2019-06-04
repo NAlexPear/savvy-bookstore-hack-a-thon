1ba752d22a40088da694f2db242343cd8b8135e7
Shantell Barge <Shantellbarge@gmail.com>
2019-06-03 17:29:16 -0700

    changestotitle

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
+import { type } from 'os';
+
 const books = [
     {
         'id': 1,
-        'name': 'Lasagna: A Retrospective',
-        'author': 'Garfield',
-        'pictureUrl': 'http://graphics8.nytimes.com/images/2015/10/15/dining/15RECIPE20DIN/15RECIPE20DIN-articleLarge.jpg',
+        'title': 'Lasagna: A Retrospective',
+        'creator': 'Garfield',
+        'image': 'http://graphics8.nytimes.com/images/2015/10/15/dining/15RECIPE20DIN/15RECIPE20DIN-articleLarge.jpg',
         'price': 24,
         'selling_points': [
             'Lasagna is delicious.',
             'The essential guide to Italian casseroles of all types.',
             "Real G's move silent, like Lasagna. -Lil Wayne"
+        
         ]
     },
     {
         'id': 2,
-        'name': 'Cryptonomicon',
-        'author': 'Neal Stephenson',
-        'pictureUrl': 'https://images-na.ssl-images-amazon.com/images/I/41K%2Bt2TSopL._SX304_BO1,204,203,200_.jpg',
+        'title': 'Cryptonomicon',
+        'creator': 'Neal Stephenson',
+        'image': 'https://images-na.ssl-images-amazon.com/images/I/41K%2Bt2TSopL._SX304_BO1,204,203,200_.jpg',
         'price': 200,
         'selling_points': [
             'Something something bitcoin',
             'I like this -Elon Musk' ,
+        
         ]
     },
 ];
@@ -28,9 +32,9 @@ function Book(book){
     return `
         <div>
             <h1>${book.name}</h1>
-            <h3>${book.author}</h3>
+            <h3>${book.creator}</h3>
             <p> Price:$${book.price}</p>
-            <img src="${book.pictureUrl}">
+            <img src="${book.image}">
             <ul>
               ${book.selling_points.map((sellingPoint) => `<li>${sellingPoint}</li>`).join('')}
             </ul>
