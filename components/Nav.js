8b61c20f1dc48c0bb298801220bca3ce06984a64
manavm1990 <manavm@visionwebsoft.com>
2019-10-08 13:17:52 -0500

    ✨Use state in Nav component.

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,8 @@
-export default function() {
+export default function(st) {
   return `<nav>
   <ul>
-    <li><a href="#">Books</a></li>
-    <li><a href="#">Albums</a></li>
+    <li><a href="#">${st.links[0]}</a></li>
+    <li><a href="#">${st.links[1]}</a></li>
   </ul>
 </nav>`
-};
+}
