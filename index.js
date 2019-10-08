24086afdab5e477f8ae78209c01e3564ffeac3c9
laura sumner <laurasumner@lauras-MacBook-Pro.local>
2019-10-07 20:56:12 -0500

    Hello

--- index.js
+++ index.js
@@ -19,7 +19,20 @@ const state = {
         "The essential guide to Italian casseroles of all types.",
         "Real G's move silent, like Lasagna. -Lil Wayne"
       ]
-    } ]
+    },
+    {
+      id: 2,
+      name: "Jumping Rope for Dummies",
+      author: "Probably Not Garfield",
+      pictureUrl:"http://i.imgur.com/vk9W3gV.jpg",
+      sellingPoints:
+      ["Cats can't jump rope but DUMMIES can",
+      "It may be dumb but it's a lot of fun",
+    "A great entertainment for the kids/cats"],
+      price: 500000
+    }
+    ]
+
 };
 
 function render(state = state){
